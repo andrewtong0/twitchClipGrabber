5e779e9e023cc3b716d6ccb0ef86c4293487048f
Andrew Tong <andrewtong0@gmail.com>
2022-01-19 16:22:26 -0800

    Added ability to copy video source URL for Reddit videos

--- popup.js
+++ popup.js
@@ -21,3 +21,32 @@ getImage.onclick = function() {
       });
   });
 };
+
+getRedditVideoUrl.onclick = function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code:"try {" +
+                    "vidURL = document.getElementsByTagName('video')[0].getElementsByTagName('source')[0].src;" +
+                    "}" +
+                    "catch(error) {" +
+                    "vidURL = null" +
+                    "}" +
+                    "vidURL"},
+            function (result) {
+                let url = result[0];
+                if (url === null){
+                    let error = document.getElementById('errorMsg');
+                    error.innerHTML = "No video clip found";
+                }
+                else {
+                    let clipboardEl = document.createElement('textarea');
+                    clipboardEl.textContent = `"${url}" `;
+                    document.body.appendChild(clipboardEl);
+                    clipboardEl.select();
+                    document.execCommand('copy');
+                    document.body.removeChild(clipboardEl);
+                }
+            });
+    });
+};
