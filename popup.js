cf404ab8c2fc068345b8815aa81e9721afdc21ef
Andrew Tong <andrewtong0@gmail.com>
2022-01-20 17:27:58 -0800

    Added title copying for easier file naming

--- popup.js
+++ popup.js
@@ -28,20 +28,25 @@ getRedditVideoUrl.onclick = function() {
             tabs[0].id,
             {code:"try {" +
                     "vidURL = document.getElementsByTagName('video')[0].getElementsByTagName('source')[0].src;" +
+                    "vidTitle = document.getElementsByTagName('h1')[0].innerText;" +
+                    "concatResult = `${vidURL} ${vidTitle}`;" +
                     "}" +
                     "catch(error) {" +
                     "vidURL = null" +
+                    "vidTitle = null" +
                     "}" +
-                    "vidURL"},
+                    "concatResult"},
             function (result) {
-                let url = result[0];
+                let returnStringArray = result[0].toString().split(/\s(.+)/);
+                let url = returnStringArray[0];
+                let title = returnStringArray[1].replace(/[^\w\s]/gi, "");
                 if (url === null){
                     let error = document.getElementById('errorMsg');
                     error.innerHTML = "No video clip found";
                 }
                 else {
                     let clipboardEl = document.createElement('textarea');
-                    clipboardEl.textContent = `"${url}" `;
+                    clipboardEl.textContent = `"${url}" ${title}`;
                     document.body.appendChild(clipboardEl);
                     clipboardEl.select();
                     document.execCommand('copy');
